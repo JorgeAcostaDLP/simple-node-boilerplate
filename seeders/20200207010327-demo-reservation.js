05dd6912aa8db24eb742f3fcff18476816f97d9b
Jorge Acosta <jh.acostadelapena@gmail.com>
2020-02-13 23:13:46 -0500

    addeed string to Down action on seeders

--- seeders/20200207010327-demo-reservation.js
+++ seeders/20200207010327-demo-reservation.js
@@ -7,18 +7,18 @@ module.exports = {
         name: 'John Cena',
         slot: new Date(Date.UTC(2020, 1, 21, 17)),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
         name: 'Not John Cena',
         slot: new Date(Date.UTC(2020, 1, 21, 18, 30)),
         createdAt: new Date(),
-        updatedAt: new Date()
-      }
+        updatedAt: new Date(),
+      },
     ]);
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Reservations', null, {});
-  }
+    return queryInterface.bulkDelete('Reservations', 'null', {});
+  },
 };
