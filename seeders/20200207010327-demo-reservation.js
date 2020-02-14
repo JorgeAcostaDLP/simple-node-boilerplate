93f5f1eca9f29121dc5e456e7e20ad33ca5b41a6
Jorge Acosta <jh.acostadelapena@gmail.com>
2020-02-13 23:17:17 -0500

    production variable enabled

--- seeders/20200207010327-demo-reservation.js
+++ seeders/20200207010327-demo-reservation.js
@@ -19,6 +19,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Reservations', 'null', {});
+    return queryInterface.bulkDelete('Reservations', null, {});
   },
 };
