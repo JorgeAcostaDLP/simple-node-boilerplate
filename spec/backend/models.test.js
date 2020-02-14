43be41b26fb06a4336549ac98cb996e4450f7578
Jorge Acosta <jh.acostadelapena@gmail.com>
2020-02-13 23:29:05 -0500

    changed env to back to development

--- spec/backend/models.test.js
+++ spec/backend/models.test.js
@@ -5,48 +5,48 @@ test('Reservation.all() should return an array', async () => {
   expect(result.length).toEqual(2);
 });
 
-// test('New Reservations can be created', async () => {
-//   await Reservation.newReservation(
-//     'Jorge',
-//     new Date(Date.UTC(2020, 1, 21, 18, 30)),
-//     new Date(Date.UTC(2020, 1, 21, 18, 0))
-//   );
-//   let result = await Reservation.all();
-//   expect(result.length).toEqual(3);
-// });
+test('New Reservations can be created', async () => {
+  await Reservation.newReservation(
+    'Jorge',
+    new Date(Date.UTC(2020, 1, 21, 18, 30)),
+    new Date(Date.UTC(2020, 1, 21, 18, 0))
+  );
+  let result = await Reservation.all();
+  expect(result.length).toEqual(3);
+});
 
-// test('Check conflict returns true if less than 10 tables taken', async () => {
-//   let i = 0;
-//   while (i < 10) {
-//     await Reservation.newReservation(
-//       'Jorge',
-//       new Date(Date.UTC(2020, 1, 21, 18, 30)),
-//       new Date(Date.UTC(2020, 1, 21, 18, 0))
-//     );
-//     i++;
-//   }
-//   let conflictCheck = await Reservation.checkConflicts(
-//     new Date(Date.UTC(2020, 1, 21, 18, 30)),
-//     new Date(Date.UTC(2020, 1, 21, 18, 0))
-//   );
+test('Check conflict returns true if less than 10 tables taken', async () => {
+  let i = 0;
+  while (i < 10) {
+    await Reservation.newReservation(
+      'Jorge',
+      new Date(Date.UTC(2020, 1, 21, 18, 30)),
+      new Date(Date.UTC(2020, 1, 21, 18, 0))
+    );
+    i++;
+  }
+  let conflictCheck = await Reservation.checkConflicts(
+    new Date(Date.UTC(2020, 1, 21, 18, 30)),
+    new Date(Date.UTC(2020, 1, 21, 18, 0))
+  );
 
-//   expect(conflictCheck).toEqual(true);
-// });
+  expect(conflictCheck).toEqual(true);
+});
 
-// test('No more than 10 tables at a given time', async () => {
-//   let i = 0;
-//   while (i < 10) {
-//     await Reservation.newReservation(
-//       'Testing',
-//       new Date(Date.UTC(2020, 1, 21, 18, 30)),
-//       new Date(Date.UTC(2020, 1, 21, 18, 0))
-//     );
-//     i++;
-//   }
-//   const test = await Reservation.newReservation(
-//     'test',
-//     new Date(Date.UTC(2020, 1, 21, 18, 30)),
-//     new Date(Date.UTC(2020, 1, 21, 18, 0))
-//   );
-//   expect(test).toEqual(undefined);
-// });
+test('No more than 10 tables at a given time', async () => {
+  let i = 0;
+  while (i < 10) {
+    await Reservation.newReservation(
+      'Testing',
+      new Date(Date.UTC(2020, 1, 21, 18, 30)),
+      new Date(Date.UTC(2020, 1, 21, 18, 0))
+    );
+    i++;
+  }
+  const test = await Reservation.newReservation(
+    'test',
+    new Date(Date.UTC(2020, 1, 21, 18, 30)),
+    new Date(Date.UTC(2020, 1, 21, 18, 0))
+  );
+  expect(test).toEqual(undefined);
+});
