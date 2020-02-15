d9d36329eef599ccb869814dbbc896627b5c87a0
Jorge Acosta <jh.acostadelapena@gmail.com>
2020-02-14 19:43:40 -0500

    fixed tests

--- spec/backend/models.test.js
+++ spec/backend/models.test.js
@@ -2,17 +2,17 @@ const { Reservation } = require('../../models');
 
 test('Reservation.all() should return an array', async () => {
   const result = (await Reservation.all()) || [];
-  expect(result.length).toEqual(2);
+  expect(result.length).toEqual(10);
 });
 
 test('New Reservations can be created', async () => {
   await Reservation.newReservation(
     'Jorge',
-    new Date(Date.UTC(2020, 1, 21, 18, 30)),
-    new Date(Date.UTC(2020, 1, 21, 18, 0))
+    new Date(Date.UTC(2020, 1, 1, 18, 30)),
+    new Date(Date.UTC(2020, 1, 1, 18, 0))
   );
   let result = await Reservation.all();
-  expect(result.length).toEqual(3);
+  expect(result.length).toEqual(11);
 });
 
 test('Check conflict returns true if less than 10 tables taken', async () => {
@@ -20,14 +20,14 @@ test('Check conflict returns true if less than 10 tables taken', async () => {
   while (i < 10) {
     await Reservation.newReservation(
       'Jorge',
-      new Date(Date.UTC(2020, 1, 21, 18, 30)),
-      new Date(Date.UTC(2020, 1, 21, 18, 0))
+      new Date(Date.UTC(2020, 1, 1, 18, 30)),
+      new Date(Date.UTC(2020, 1, 1, 18, 0))
     );
     i++;
   }
   let conflictCheck = await Reservation.checkConflicts(
-    new Date(Date.UTC(2020, 1, 21, 18, 30)),
-    new Date(Date.UTC(2020, 1, 21, 18, 0))
+    new Date(Date.UTC(2020, 1, 1, 18, 30)),
+    new Date(Date.UTC(2020, 1, 1, 18, 0))
   );
 
   expect(conflictCheck).toEqual(true);
@@ -38,15 +38,15 @@ test('No more than 10 tables at a given time', async () => {
   while (i < 10) {
     await Reservation.newReservation(
       'Testing',
-      new Date(Date.UTC(2020, 1, 21, 18, 30)),
-      new Date(Date.UTC(2020, 1, 21, 18, 0))
+      new Date(Date.UTC(2020, 1, 10, 18, 30)),
+      new Date(Date.UTC(2020, 1, 10, 18, 0))
     );
     i++;
   }
   const test = await Reservation.newReservation(
     'test',
-    new Date(Date.UTC(2020, 1, 21, 18, 30)),
-    new Date(Date.UTC(2020, 1, 21, 18, 0))
+    new Date(Date.UTC(2020, 1, 10, 18, 30)),
+    new Date(Date.UTC(2020, 1, 10, 18, 0))
   );
   expect(test).toEqual(undefined);
 });
