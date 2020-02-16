5bc962108f26767c8f59b77d4dd64e8032260b76
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2020-02-15 21:21:15 -0500

    ABC also added some description to main.js and to dos

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,19 @@
 window.addEventListener('load', () => {
   getReservations();
 });
+//Display existing reservations on load
 let previous = [];
+
+//get
 const getReservations = () => {
   fetch('/reservations')
     .then(response => response.json())
     .then(data => {
       let upcoming = [];
       data.map(el => {
+        //do not load old reservations
         if (!previous.includes(el.id)) {
+          //date split
           slotSplit = el.slot.split('T');
           slotMonth = slotSplit[0].slice(5, 7);
           slotDay = slotSplit[0].slice(8, 10);
@@ -52,15 +57,18 @@ const getReservations = () => {
               month = 'December';
               break;
           }
+          //create nodes with date info for new reservation
           upcoming.push(
             document.createTextNode(
               `${el.name} on ${month} ${slotDay} at ${slotSplit[1].slice(0, 5)}`
             )
           );
+          //add new reservation to existing reservations
           previous.push(el.id);
           previous.push(el.name);
         }
       });
+      //append list items to portal display
       upcoming.map(node => {
         let listItem = document.createElement('LI');
         listItem.appendChild(node);
@@ -69,14 +77,18 @@ const getReservations = () => {
     });
 };
 
+//get form for posting/prevent defaulting
 const form = $('#reservation');
 form.on('submit', submitHandler);
 
+//Post
 function submitHandler(e) {
   e.preventDefault();
   $.ajax({
     url: '/reservations',
     type: 'POST',
-    data: form.serialize(),
+    data: form.serialize()
+    //clears form data and displays latest reservation
   }).then(location.reload());
 }
+//to do display 'reservation time not available' if reservation.addReservation fails
