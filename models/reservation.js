f02294357e75b59a7111ca8078a420b42fd403cb
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2020-02-15 21:32:57 -0500

    Added "todos" and more descriptions

--- models/reservation.js
+++ models/reservation.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     static async all() {
       return await this.findAll();
     }
-
+    //conflict check works by adding the tables with the required slot with the previous half hour slot to avoid more than 10 tables at a time
     static async checkConflicts(slot, prevHalfHour) {
       const tablesTaken = await this.findAll({
         where: { slot: slot }
@@ -19,16 +19,21 @@ module.exports = (sequelize, DataTypes) => {
       });
 
       let tablesTakenTotal = tablesTaken.length + previousHalfHour.length;
-
+      //no conflict return false, reservation can be made.
       if (tablesTakenTotal < 10) return false;
+      //conflict return true
       else return true;
     }
 
     static async newReservation(name, slot, prev) {
       const conflictCheck = await this.checkConflicts(slot, prev);
       if (conflictCheck === true) {
+        //if there is a conflict console.log message.
+        //todo: return message for front end display (Please try a different time)
         console.log('No reservations available at this time');
       }
+      //if no conflict create reservation, display message
+      //todo: return 'reservation succesfully created' for front end display
       if (conflictCheck === false) {
         await this.create({
           name: name,
--- routes/reservations/index.js
+++ routes/reservations/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { Reservation } = require('../../models');
 
+//get all reservations for display, they are available at /reservations
 router.get('/', async (req, res) => {
   res.json(await Reservation.all());
 });
 
+//todo: esplit into functions for utils for front end testing
 router.post('/', async (req, res, next) => {
   let slotSplit = req.body.slot.split(':');
+  //convert strings to values to avoid creating dates with strings
   let month = req.body.month;
   switch (month) {
     case 'January':
@@ -47,9 +50,12 @@ router.post('/', async (req, res, next) => {
       month = 11;
       break;
   }
+  //create new date (UTC works)
+  //todo: add front end validations to avoid creating weird dates (month/28/29/30 or 31 possible dates... etc)
   let slot = new Date(
     Date.UTC(2020, month, +req.body.day, +slotSplit[0], +slotSplit[1])
   );
+  //create previous half hour for conflict check (no more than 10 reservations at a time)
   let prevHalf, prevFull;
   if (+slotSplit[1] === 30) {
     prevHalf = 0;
@@ -58,11 +64,12 @@ router.post('/', async (req, res, next) => {
     prevHalf = 30;
     prevFull = +slotSplit[0] - 1;
   }
-
+  //no more than ten tables available at any given time, reservations can be made every half hour
   prevHalfHour = new Date(
     Date.UTC(2020, month, +req.body.day, prevFull, prevHalf)
   );
-
+  //newReservation() takes in name, slot and the previous half hour slot for conflict check
+  //todo: return error message if no table available.
   await Reservation.newReservation(req.body.name, slot, prevHalfHour);
 });
 module.exports = router;
